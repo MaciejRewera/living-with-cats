c10d3a5e8e6d9853556a50536108081bdea88910
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-07-16 20:59:54 +0100

    Implement Eq instance for Cat class

--- src/main/scala/rewera/livingwithcats/printable/Cat.scala
+++ src/main/scala/rewera/livingwithcats/printable/Cat.scala
@@ -1,7 +1,7 @@
 package rewera.livingwithcats.printable
 
-import cats.Show
 import cats.implicits._
+import cats.{Eq, Show}
 
 case class Cat(name: String, age: Int, color: String)
 
@@ -12,5 +12,11 @@ object Cat {
     val age   = cat.age.show
     val color = cat.color.show
     s"$name is a $age year-old $color cat."
+  }
+
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
+    cat1.name === cat2.name &&
+    cat1.age === cat2.age &&
+    cat1.color === cat2.color
   }
 }
--- src/main/scala/rewera/livingwithcats/printable/Main.scala
+++ src/main/scala/rewera/livingwithcats/printable/Main.scala
@@ -24,4 +24,20 @@ object Main extends App {
   println(123.show)
 
   println(cat.show)
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  println("-------------")
+  println(cat1 === cat2)
+  println(cat2 === cat1)
+  println(cat1 === cat1)
+  println(cat2 === cat2)
+  println("-------------")
+  println(optionCat1 === optionCat2)
+  println(optionCat2 === optionCat1)
+  println(optionCat1 === optionCat1)
+  println(optionCat2 === optionCat2)
 }
