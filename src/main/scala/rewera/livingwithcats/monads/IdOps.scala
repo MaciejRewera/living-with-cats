cec157280b328e1a6ebba60e27c7a7a17a0ecedc
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-07-29 18:10:18 +0100

    Implement Monadic Secret Identities exercise [4.3.1]

---
+++ src/main/scala/rewera/livingwithcats/monads/IdOps.scala
@@ -0,0 +1,11 @@
+package rewera.livingwithcats.monads
+
+import cats.Id
+
+trait IdOps {
+  def pure[A](value: A): Id[A] = value
+
+  def flatMap[A, B](value: Id[A])(func: A => Id[B]): Id[B] = func(value)
+
+  def map[A, B](value: Id[A])(func: A => B): Id[B] = func(value)
+}
