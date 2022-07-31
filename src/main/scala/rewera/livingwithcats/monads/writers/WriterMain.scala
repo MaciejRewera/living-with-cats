371187e61b9d908c9a0b7f50bb4b731ce450c1d9
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-07-31 14:53:13 +0100

    Implement Writer exercise 'Show Your Working' [4.7.3]

---
+++ src/main/scala/rewera/livingwithcats/monads/writers/WriterMain.scala
@@ -0,0 +1,44 @@
+package rewera.livingwithcats.monads.writers
+
+import cats.data.Writer
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+import scala.util.Random
+import cats.syntax.applicative._
+import cats.instances.vector._
+
+object WriterMain {
+
+  def main(args: Array[String]): Unit = {
+
+    val writerResults = Await.result(Future.sequence(Seq(
+      Future(factorial(5)),
+      Future(factorial(5))
+    )), 5.seconds)
+
+    val results = writerResults.map(_.value)
+    val logs = writerResults.map(_.written)
+
+    results.foreach(println)
+
+    logs.foreach(_.foreach(println))
+    logs.flatten.foreach(println)
+  }
+
+  private def slowly[A](body: => A): A = try body finally Thread.sleep(1 + Random.nextLong(100))
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  private def factorial(n: Int): Logged[Int] =
+    slowly {
+      if (n == 0) 1.pure[Logged]
+      else factorial(n - 1).mapBoth { (logs, value) =>
+        val result = value * n
+        val newLogs = logs :+ s"factorial $n is $result"
+
+        (newLogs, result)
+      }
+    }
+}
