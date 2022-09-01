4707049a4079186a7056bae4ccc55ee5a07f0274
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-09-01 13:57:58 +0100

    Implement contramap method for Printable

--- src/main/scala/rewera/livingwithcats/printable/Printable.scala
+++ src/main/scala/rewera/livingwithcats/printable/Printable.scala
@@ -1,7 +1,11 @@
 package rewera.livingwithcats.printable
 
-trait Printable[A] {
+trait Printable[A] { self =>
   def format(value: A): String
+
+  def contramap[B](func: B => A): Printable[B] = new Printable[B] {
+    override def format(value: B): String = self.format(func(value))
+  }
 }
 
 object Printable {
